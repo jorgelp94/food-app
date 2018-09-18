a7c316a81711d848aa629af4022bc6fa51352256
Jorge Perales <luis.perales@dextratech.com>
2018-09-18 14:52:26 -0500

    Initial Commit

---
+++ food-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  food-app
+//
+//  Created by Jorge Luis Perales Saldaña on 9/18/18.
+//  Copyright © 2018 Jorge Luis Perales Saldaña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
